4d8e3c28faf23ab8d31287fc3c04f8ce34a49c78
WinserEX <winser500@gmail.com>
2023-03-06 22:56:27 -0400

    Improved form validation capabilities and finally fixed logical problems

--- index.js
+++ index.js
@@ -4,29 +4,61 @@ let lbsKgs = (x) => {
     return `${res} Kg`;          
 }
 
+function validateForm() {
+    let x = document.forms["myForm"]["num"].value;
+    let y = document.forms["myForm"]["nombre"].value;
+    if (x == "" || y == "") {
+      alert("Los campos no pueden estar en blanco");
+      return false;
+    }
+}
+
 let con = document.querySelector('.container')
 document.querySelector(".btn").addEventListener("click", function(event){
     event.preventDefault()
+
+    validateForm();
+
     let num = document.querySelector(".num");
     let nombre = document.querySelector(".nombre");
     let valNum = document.querySelector(".valNum");
     let valCol = document.querySelector(".valCol");
+    //test 1
+    console.log(`Num has child: ${valNum.hasChildNodes()}`)
+    console.log(`Name has child: ${valCol.hasChildNodes()}`)
+
     if (valNum.hasChildNodes() == true || valCol.hasChildNodes() == true) {
         valNum.removeChild(valNum.childNodes[0])
         valCol.removeChild(valCol.childNodes[0])
+        console.log("Child nodes")
     } else {
         console.log("No child nodes")
     }
-    valNum.append(`${num.value} lbs son ${lbsKgs(num.value)}`);
-    if (num.value > 250) {
+
+    if (num.value != null) {
+        valNum.append(`${num.value} lbs son ${lbsKgs(num.value)}`);
+    } 
+    else {
+        valNum.append(`Por favor agrega la medida para poder convertirla`);
+    }
+
+    if (num.value > 250 && nombre.value != "") {
         valCol.append(`${nombre.value}, si este es tu peso lo mejor es que cuides más tu salud.`);
     } 
+    else if (nombre.value == "") {
+        valCol.append(`Agrega tu nombre por favor`);
+    }
     else {
         valCol.append(`Gracias por usar este convertidor ${nombre.value}`);
     }
-    num.value = null;
+    num.value = 0;
     nombre.value = "";
+    //test 2
+    console.log(`Num has child: ${valNum.hasChildNodes()}`)
+    console.log(`Name has child: ${valCol.hasChildNodes()}`)
   });
+
+
 
   //1 libra == 0.453592
 
